169e3f826d14ed2d93267b9c87619daa1ea1614c
Robin Gower <robin.git@infonomics.ltd.uk>
2021-10-12 17:30:14 +0200

    Adjust styles adding triangulation to other pages

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 mkdir -p build
 cp source/*.html source/*.css source/*.js source/*.js.map build
 pandoc -o build/standards.html --template source/page.html.template source/standards.md
+pandoc -o build/tools.html --template source/page.html.template source/tools.md
+pandoc -o build/guides.html --template source/page.html.template source/guides.md
