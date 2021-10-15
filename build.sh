245d2a9b04ca628cc4c7c627917561fd749557ab
Robin Gower <robin.git@infonomics.ltd.uk>
2021-10-15 16:29:06 +0200

    Extract head and tail partial to DRY-up templates

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mkdir -p build
-cp source/*.html source/*.css source/*.js source/*.js.map build
+cp source/*.css source/*.js source/*.js.map build
+echo | pandoc -o build/index.html --template source/index.html --metadata title="CSV on the Web"
 pandoc -o build/standards.html --template source/page.html.template source/standards.md
 pandoc -o build/tools.html --template source/tools.html.template source/tools.md
 pandoc -o build/guides.html --template source/page.html.template source/guides.md
