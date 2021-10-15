24eed78ec00047a1d1a712c830688675cea6be8a
Robin Gower <robin.git@infonomics.ltd.uk>
2021-10-15 18:09:57 +0200

    Better layout for standards page

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 mkdir -p build
 cp source/*.css source/*.js source/*.js.map build
 echo | pandoc -o build/index.html --template source/index.html --metadata title="CSV on the Web"
-pandoc -o build/standards.html --template source/page.html.template source/standards.md
+pandoc -o build/standards.html --template source/page.html.template -f markdown-native_divs source/standards.html --metadata title="Standards"
 pandoc -o build/tools.html --template source/tools.html.template source/tools.md
 pandoc -o build/guides.html --template source/page.html.template source/guides.md
